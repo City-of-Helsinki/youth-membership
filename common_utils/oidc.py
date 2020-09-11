8603b8087fb78b3c7165f1a28c6be7ac9c5e5511
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-09-11 14:46:51 +0300

    Add logic for fetching profile API token
    - Add logic capable of excahing an authorization token
      into a API token for Helsinki profile
    - Add env configuration variables for Helsinki profile
      scope and callback url
    
    Refs YM-284

--- common_utils/oidc.py
+++ common_utils/oidc.py
@@ -1,4 +1,7 @@
+import requests
+from django.conf import settings
 from helusers.oidc import ApiTokenAuthentication
+from requests_oauthlib import OAuth2Session
 
 
 class GraphQLApiTokenAuthentication(ApiTokenAuthentication):
@@ -18,3 +21,68 @@ class GraphQLApiTokenAuthentication(ApiTokenAuthentication):
             return None
         user, auth = user_auth_tuple
         return user
+
+
+class TunnistamoTokenExchange:
+    """Exchanges an authorization code with Tunnistamo into API token for open-city-profile."""
+
+    def __init__(self):
+        self.oidc_endpoint = settings.SOCIAL_AUTH_TUNNISTAMO_OIDC_ENDPOINT
+        self.client_id = settings.SOCIAL_AUTH_TUNNISTAMO_KEY
+        self.client_secret = settings.SOCIAL_AUTH_TUNNISTAMO_SECRET
+        self.scope = settings.HELSINKI_PROFILE_AUTH_SCOPE
+        self.callback_url = settings.HELSINKI_PROFILE_AUTH_CALLBACK_URL
+        self.timeout = 5
+
+    def fetch_api_token(self, authorization_code: str) -> str:
+        """Exchanges the authorization code into a API token that can access open-city-profile API."""
+        oidc_conf = self.get_oidc_config()
+        session = OAuth2Session(
+            client_id=self.client_id,
+            redirect_uri=self.callback_url,
+            scope=f"openid {self.scope}",
+        )
+        authorization_url, state = session.authorization_url(
+            oidc_conf["authorization_endpoint"]
+        )
+        redirect_response = (
+            f"{self.callback_url}?code={authorization_code}&state={state}"
+        )
+        session.fetch_token(
+            token_url=oidc_conf["token_endpoint"],
+            authorization_response=redirect_response,
+            client_secret=self.client_secret,
+            include_client_id=True,
+            timeout=self.timeout,
+        )
+        response = session.get(self.oidc_endpoint + "/api-tokens", timeout=self.timeout)
+        response.raise_for_status()
+        api_tokens = response.json()
+
+        if self.scope in api_tokens:
+            return api_tokens[self.scope]
+
+        raise Exception(f"Token for scope {self.scope} not available in response.")
+
+    def get_authorization_token_url(self):
+        """Return the url, which will generate a authorization code when visited."""
+        oidc_conf = self.get_oidc_config()
+        session = OAuth2Session(
+            client_id=self.client_id,
+            redirect_uri=self.callback_url,
+            scope=f"openid {self.scope}",
+        )
+
+        authorization_url, state = session.authorization_url(
+            oidc_conf["authorization_endpoint"]
+        )
+        return authorization_url
+
+    def get_oidc_config(self):
+        return self.get(self.oidc_endpoint + "/.well-known/openid-configuration").json()
+
+    def get(self, url: str) -> requests.Response:
+        headers = {"accept": "application/json"}
+        response = requests.get(url, headers=headers, timeout=self.timeout)
+        response.raise_for_status()
+        return response
