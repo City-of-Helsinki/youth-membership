68d2374e8c844427ee7980d27891547300654f5f
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-09-16 13:46:06 +0300

    Allow changing additional contact persons when approving a youth profile
    Refs YM-180

--- youths/tests/test_graphql_api_additional_contact_persons.py
+++ youths/tests/test_graphql_api_additional_contact_persons.py
@@ -45,6 +45,17 @@ UPDATE_MUTATION = Template(
 )
 
 
+APPROVAL_MUTATION = Template(
+    """
+    mutation UpdateMyYouthProfile($$input: ApproveYouthProfileMutationInput!) {
+        approveYouthProfile(input: $$input) ${query}
+    }
+    """
+).substitute(
+    query=ADDITIONAL_CONTACT_PERSONS_QUERY_BASE.substitute(query_object="youthProfile")
+)
+
+
 def test_normal_user_can_add_additional_contact_persons(rf, user_gql_client):
     YouthProfileFactory(user=user_gql_client.user)
     acpd = AdditionalContactPersonDictFactory()
@@ -183,3 +194,49 @@ def test_normal_user_can_query_additional_contact_persons(
         }
     }
     assert dict(executed["data"]) == expected_data
+
+
+def test_profile_approval_allows_changing_contact_persons(
+    rf, anon_user_gql_client, youth_profile
+):
+    request = rf.post("/graphql")
+    request.user = anon_user_gql_client.user
+
+    acp_new_data = AdditionalContactPersonDictFactory()
+    acp_update = AdditionalContactPersonFactory(youth_profile=youth_profile)
+    acp_update_values = AdditionalContactPersonDictFactory()
+    acp_remove = AdditionalContactPersonFactory(youth_profile=youth_profile)
+
+    variables = {
+        "input": {
+            "approvalToken": youth_profile.approval_token,
+            "approvalData": {
+                "addAdditionalContactPersons": [acp_new_data],
+                "updateAdditionalContactPersons": [
+                    {
+                        "id": to_global_id(
+                            type="AdditionalContactPersonNode", id=acp_update.pk
+                        ),
+                        **acp_update_values,
+                    }
+                ],
+                "removeAdditionalContactPersons": [
+                    to_global_id(type="AdditionalContactPersonNode", id=acp_remove.pk)
+                ],
+            },
+        }
+    }
+
+    anon_user_gql_client.execute(
+        APPROVAL_MUTATION, context=request, variables=variables
+    )
+
+    assert not youth_profile.additional_contact_persons.filter(
+        pk=acp_remove.pk
+    ).exists()
+    assert youth_profile.additional_contact_persons.filter(
+        pk=acp_update.pk, first_name=acp_update_values["firstName"]
+    ).exists()
+    assert youth_profile.additional_contact_persons.exclude(
+        pk__in=[acp_update.pk, acp_remove.pk]
+    ).exists()
