bfe6fe71113ed57f6fe46bbbdad65b985b4bcb8b
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-10-22 09:30:10 +0300

    Generate youth-membership notification templates
    Refs YM-361

--- youths/utils.py
+++ youths/utils.py
@@ -3,9 +3,12 @@ from datetime import date
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
+from django.db import transaction
+from django_ilmoitin.models import NotificationTemplate
 from graphql_relay import from_global_id
 
 from common_utils.exceptions import InvalidEmailFormatError
+from youths.enums import NotificationType
 from youths.models import AdditionalContactPerson, YouthProfile
 
 
@@ -63,3 +66,78 @@ def delete_contact_persons(youth_profile: YouthProfile, data):
         AdditionalContactPerson.objects.get(
             youth_profile=youth_profile, pk=remove_id
         ).delete()
+
+
+@transaction.atomic
+def generate_notifications():
+    """Creates Youth Profile notifications if they don't already exist."""
+
+    if not NotificationTemplate.objects.filter(
+        type=NotificationType.YOUTH_PROFILE_CONFIRMATION_NEEDED.value
+    ).exists():
+        template = NotificationTemplate(
+            type=NotificationType.YOUTH_PROFILE_CONFIRMATION_NEEDED.value
+        )
+        fi_subject = "Vahvista nuorisojäsenyys"
+        fi_html = (
+            "Hei {{ youth_profile.approver_first_name }},<br /><br />{{ youth_profile.profile.first_name }} on "
+            "pyytänyt sinua vahvistamaan nuorisojäsenyytensä. Käy antamassa vahvistus Jässäri-palvelussa käyttäen "
+            "tätä linkkiä:"
+            '<br /><br /><a href="https://jassari.test.kuva.hel.ninja/approve/{{ youth_profile.approval_token }}">'
+            "https://jassari.test.kuva.hel.ninja/approve/{{ youth_profile.approval_token }}</a><br /><br /><i>Tämä "
+            "viesti on lähetetty järjestelmistä automaattisesti. Älä vastaa tähän viestiin, sillä vastauksia ei "
+            "käsitellä.</i>"
+        )
+        fi_text = (
+            "Hei {{ youth_profile.approver_first_name }},\r\n\r\n{{ youth_profile.profile.first_name }} on pyytänyt "
+            "sinua vahvistamaan nuorisojäsenyytensä. Käy antamassa vahvistus Jässäri-palvelussa käyttäen tätä linkkiä:"
+            "\r\n\r\nhttps://jassari.test.kuva.hel.ninja/approve/{{ youth_profile.approval_token }}\r\n\r\nTämä viesti "
+            "on lähetetty järjestelmistä automaattisesti. Älä vastaa tähän viestiin, sillä vastauksia ei käsitellä."
+        )
+        template.set_current_language("fi")
+        template.subject = fi_subject
+        template.body_html = fi_html
+        template.body_text = fi_text
+        template.set_current_language("sv")
+        template.subject = fi_subject + " SV TRANSLATION NEEDED"
+        template.body_html = fi_html + "<p>SV TRANSLATION NEEDED</p>"
+        template.body_text = fi_text + "<p>SV TRANSLATION NEEDED</p>"
+        template.set_current_language("en")
+        template.subject = fi_subject + " EN TRANSLATION NEEDED"
+        template.body_html = fi_html + "<p>EN TRANSLATION NEEDED</p>"
+        template.body_text = fi_text + "<p>EN TRANSLATION NEEDED</p>"
+        template.save()
+
+    if not NotificationTemplate.objects.filter(
+        type=NotificationType.YOUTH_PROFILE_CONFIRMED.value
+    ).exists():
+        template = NotificationTemplate(
+            type=NotificationType.YOUTH_PROFILE_CONFIRMED.value
+        )
+        fi_subject = "Nuorisojäsenyys vahvistettu"
+        fi_html = (
+            "Hei {{ youth_profile.profile.first_name }},\r\n<br /><br />\r\n{{ youth_profile.approver_first_name }} on "
+            "vahvistanut nuorisojäsenyytesi. Kirjaudu Jässäri-palveluun nähdäksesi omat tietosi:\r\n<br /><br />\r\n"
+            '<a href="https://jassari.test.kuva.hel.ninja">https://jassari.test.kuva.hel.ninja</a>\r\n<br /><br />\r\n'
+            "<i>Tämä viesti on lähetetty järjestelmästä automaattisesti. Älä vastaa tähän viestiin, sillä vastauksia "
+            "ei käsitellä.</i>"
+        )
+        fi_text = (
+            "Hei {{ youth_profile.profile.first_name }},\r\n\r\n{{ youth_profile.approver_first_name }} on vahvistanut "
+            "nuorisojäsenyytesi. Kirjaudu Jässäri-palveluun nähdäksesi omat tietosi:\r\n\r\n"
+            "https://jassari.test.kuva.hel.ninja\r\n\r\nTämä viesti on lähetetty järjestelmästä automaattisesti. Älä "
+            "vastaa tähän viestiin, sillä vastauksia ei käsitellä."
+        )
+        template.set_current_language("fi")
+        template.subject = fi_subject
+        template.body_html = fi_html
+        template.body_text = fi_text
+        template.set_current_language("sv")
+        template.subject = fi_subject + " SV TRANSLATION NEEDED"
+        template.body_html = fi_html + "<p>SV TRANSLATION NEEDED</p>"
+        template.body_text = fi_text + "<p>SV TRANSLATION NEEDED</p>"
+        template.set_current_language("en")
+        template.subject = fi_subject + " EN TRANSLATION NEEDED"
+        template.body_html = fi_html + "<p>EN TRANSLATION NEEDED</p>"
+        template.body_text = fi_text + "<p>EN TRANSLATION NEEDED</p>"
+        template.save()
