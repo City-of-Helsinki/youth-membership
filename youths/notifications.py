c5168c4b23bf26ea226602e50063453a9ed74361
Juha Louhiranta <juha.louhiranta@anders.fi>
2020-11-05 16:47:23 +0200

    Update notification dummy context
    This makes preview functionality for the current
    notification templates.
    
    Refs YM-417, YM-418

--- youths/notifications.py
+++ youths/notifications.py
@@ -1,4 +1,4 @@
-from django_ilmoitin.dummy_context import dummy_context
+from django_ilmoitin.dummy_context import COMMON_CONTEXT, dummy_context
 from django_ilmoitin.registry import notifications
 
 from .enums import NotificationType
@@ -12,4 +12,19 @@ notifications.register(
     NotificationType.YOUTH_PROFILE_CONFIRMED.label,
 )
 
-dummy_context.context.update({"youth_profile": None})
+
+dummy_context.update(
+    {
+        COMMON_CONTEXT: {
+            "youth_name": "YOUTH NAME",
+            "youth_profile": {"approver_first_name": "APPROVER FIRST NAME"},
+        },
+        NotificationType.YOUTH_PROFILE_CONFIRMATION_NEEDED.value: {
+            "youth_profile": {
+                "approver_first_name": "APPROVER FIRST NAME",
+                "approval_token": "approval_token",
+                "profile_access_token": "profile_access_token",
+            }
+        },
+    }
+)
